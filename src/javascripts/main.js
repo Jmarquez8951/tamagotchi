346e5d3f34436e71126db45567eec53ac2c73288
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-06 21:00:29 -0600

    got the play features to work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,10 @@
 import '../styles/main.scss';
 import eat from './components/eat';
+import fun from './components/play';
 
 const init = () => {
   eat.eatingFunction();
+  fun.playFunction();
 };
 
 init();
