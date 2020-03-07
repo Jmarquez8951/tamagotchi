78b0bf9bf1fe668e1468624c6e3550bb0edc97f7
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-06 21:22:31 -0600

    got the fight features to work

---
+++ src/javascripts/components/fight.js
@@ -0,0 +1,43 @@
+import util from '../helpers/util';
+import tamagotchi from '../helpers/data/tamagotchiData';
+
+const runAway = () => {
+  const myTamagotchi = tamagotchi.getTamagotchi();
+  if (myTamagotchi.strength >= 99) {
+    myTamagotchi.strength = 100;
+  } else {
+    myTamagotchi.strength += 1;
+  }
+  // eslint-disable-next-line no-use-before-define
+  fightFunction();
+};
+
+const fightButton = () => {
+  const myTamagotchi = tamagotchi.getTamagotchi();
+  if (myTamagotchi.strength <= 10) {
+    myTamagotchi.strength = 0;
+  } else {
+    myTamagotchi.strength -= 10;
+  }
+  // eslint-disable-next-line no-use-before-define
+  fightFunction();
+};
+
+const fightEvents = () => {
+  $('#run-away').click(runAway);
+  $('#fight-button').click(fightButton);
+};
+
+const fightFunction = () => {
+  const myTamagotchi = tamagotchi.getTamagotchi();
+  let domString = '';
+  domString += '<div>';
+  domString += `<p>Strength: ${myTamagotchi.strength}</p>`;
+  domString += '<button id="run-away">Bravely Runaway</button>';
+  domString += '<button id="fight-button">Commit Violence</button>';
+  domString += '</div>';
+  util.printToDom('fight', domString);
+  fightEvents();
+};
+
+export default { fightFunction };
