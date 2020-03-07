f4bcf4227ebd448afaf42c5022995f312af6e11d
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-06 21:54:59 -0600

    got the sleep buttons to work

---
+++ src/javascripts/components/sleep.js
@@ -0,0 +1,43 @@
+import util from '../helpers/util';
+import tamagotchi from '../helpers/data/tamagotchiData';
+
+const napEvent = () => {
+  const myTamagotchi = tamagotchi.getTamagotchi();
+  if (myTamagotchi.energy >= 50) {
+    myTamagotchi.energy = 100;
+  } else {
+    myTamagotchi.energy += 50;
+  }
+  // eslint-disable-next-line no-use-before-define
+  sleepFunction();
+};
+
+const slumberEvent = () => {
+  const myTamagotchi = tamagotchi.getTamagotchi();
+  if (myTamagotchi.energy >= 40) {
+    myTamagotchi.energy = 100;
+  } else {
+    myTamagotchi.energy += 60;
+  }
+  // eslint-disable-next-line no-use-before-define
+  sleepFunction();
+};
+
+const sleepEvents = () => {
+  $('#quick-nap').click(napEvent);
+  $('#deep-slumber').click(slumberEvent);
+};
+
+const sleepFunction = () => {
+  const myTamagotchi = tamagotchi.getTamagotchi();
+  let domString = '';
+  domString += '<div>';
+  domString += `<p>Energy: ${myTamagotchi.energy}</p>`;
+  domString += '<button id="quick-nap">Nap</button>';
+  domString += '<button id="deep-slumber">Deep Slumber</button>';
+  domString += '</div>';
+  util.printToDom('sleep', domString);
+  sleepEvents();
+};
+
+export default { sleepFunction };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import '../styles/main.scss';
 import eat from './components/eat';
 import fun from './components/play';
 import fight from './components/fight';
+import sleep from './components/sleep';
 
 const init = () => {
   eat.eatingFunction();
   fun.playFunction();
   fight.fightFunction();
+  sleep.sleepFunction();
 };
 
 init();
